73375d74013c27e245565a5fde34c18b253a4433
LuPhuocHung1998 <47934237+LuPhuocHung@users.noreply.github.com>
2019-11-26 14:37:50 +0700

    update

--- BackEnd/app/models/book.js
+++ BackEnd/app/models/book.js
@@ -5,7 +5,9 @@ const bookSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   name: { type: String, required: true},
   price: { type: Number, required: true},
-  bookImage: {type: String, required: true}
+  bookImage: {type: String, required: true},
+  provider: {type: String, required: true}
 });
+
 
 module.exports = mongoose.model('Book', bookSchema);
