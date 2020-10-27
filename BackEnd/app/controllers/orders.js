0f25d5fd32903e2f165a5fde3b9a58084bfe237b
LuPhuocHung1998 <47934237+LuPhuocHung@users.noreply.github.com>
2020-10-27 22:57:27 +0700

    update order

--- BackEnd/app/controllers/orders.js
+++ BackEnd/app/controllers/orders.js
@@ -1,98 +1,128 @@
 const Order = require('../models/order');
-const Cart = require('../models/cart1')
+const Cart = require('../models/cart1');
+const OrderDetail = require('../models/orderDetail');
+const books = require('./books');
+const book = require('../models/book');
 module.exports = {
     // Get Orders 
-    getOrder : (req, res, next) => {
+    getOrder: (req, res, next) => {
         Order.find()
-        .exec()
-        .then(docs => {
-            if (docs.length >= 0) {
-                res.status(200).json(docs);
-            } else {
-                res.status(404).json({
-                    message: "No Entries Found"
+            .exec()
+            .then(docs => {
+                if (docs.length >= 0) {
+                    res.status(200).json(docs);
+                } else {
+                    res.status(404).json({
+                        message: "No Entries Found"
+                    });
+                }
+            })
+            .catch(err => {
+                res.status(500).json({
+                    error: err
                 });
-            }
-        })
-        .catch(err => {
-            res.status(500).json({
-                error: err
             });
-        });
-     },
+    },
 
-   
+
 
     // Delete Order
     deleteOrder: (req, res, next) => {
-        Order.remove({_id: req.params.orderId})
-        .exec()
-        .then(result => {
-            res.status(200).json({
-                message: "Order deleted"
+        Order.remove({ _id: req.params.orderId })
+            .exec()
+            .then(result => {
+                res.status(200).json({
+                    message: "Order deleted"
+                });
+            })
+            .catch(err => {
+                res.status(500).json({
+                    error: err
+                });
             });
-        })
-        .catch(err => {
-            res.status(500).json({
-                error: err
-            });
-        });
     },
 
     // Get order by orderID
     getOrderID: (req, res, next) => {
         Order.findById(req.params.orderId)
-        .populate('book')
-        .exec()
-        .then(order => {
-            if(!order)
-            {
-                return res.status(404).json({
-                    message: "Order not found"
+            .populate('book')
+            .exec()
+            .then(order => {
+                if (!order) {
+                    return res.status(404).json({
+                        message: "Order not found"
+                    });
+                }
+                res.status(200).json(order);
+            })
+            .catch(err => {
+                res.status(500).json({
+                    error: err
                 });
-            }
-            res.status(200).json(order);
-        })
-        .catch(err => {
-            res.status(500).json({
-                error: err
             });
-        });
     },
 
     // Checkout and Create Order
-    createOrder:  (req, res, next) => {
+    createOrder: (req, res, next) => {
 
-        const cart = new Cart();
+        //  const cart = new Cart();
         // const totalPrice = cart.totalPrice ;
         // const totalQty = cart.totalQty ; 
 
-        cart.books.push(req.body.books);
-        cart.totalPrice = req.body.totalPrice;
-        cart.totalQty = req.body.totalQty;
+        // cart.books.push(req.body.books);
+        // cart.books = req.body.books;
+        // cart.totalPrice = req.body.totalPrice;
+        // cart.totalQty = req.body.totalQty;
 
         const order = new Order({
-                email: req.body.email,
-                cart: cart,
-                phone: req.body.phone,
-                address: req.body.address,
-                name: req.body.name,
-            });
-            order.save(function (err, result) {
-                if(err)
-                {
-                    res.status(500).json({
-                        error:err
-                    });
-                }else{
-                    // req.session.cart = null;
-                    res.status(200).json({
-                        // totalPrice: totalPrice,
-                        // totalQty: totalQty,
-                        message: 'Successfully bought book!'
-                    });
-                }
-            });
+            email: req.body.email,
+            totalPrice: req.body.totalPrice,
+            phone: req.body.phone,
+            address: req.body.address,
+            name: req.body.name,
+        });
+        const books = req.body.books;
+        let userId;
+        let arrayBook = [];
+        order.save(function (err, result) {
+            if (err) {
+                res.status(500).json({
+                    error: err
+                });
+            } else {
+                //   req.session.cart = null;
+                res.status(200).json({
+                    // totalPrice: totalPrice,
+                    // totalQty: totalQty,
+                    message: 'Successfully bought book!'
+                });
+            }
+        });
+
+
+        // Luu order._id vao bang orderDetail va san pham da dc order
+        let orderId;
+        if (order) {
+            orderId = order._id;
+        } else {
+            return res.json({ msg: "Error when create order!" });
         }
+
+        // Create OrderDetail
+        const newOrderDetail = {
+            orderId: orderId,
+            books: books,
+        };
+
+       // const createdOrderDetail = 
+        OrderDetail.create(newOrderDetail);
+
+        // if(createdOrderDetail) {
+        //     books.map(async (book) => {
+        //         const id = parseInt(book._id)
+        //     });
+        // }
+
+    }
 
 }
