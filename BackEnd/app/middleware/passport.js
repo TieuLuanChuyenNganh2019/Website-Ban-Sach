56d52e32808a432a0c867c548f246a32ede422b0
LuPhuocHung1998 <47934237+LuPhuocHung@users.noreply.github.com>
2020-11-10 18:16:57 +0700

    update get/books

--- BackEnd/app/middleware/passport.js
+++ BackEnd/app/middleware/passport.js
@@ -1,8 +1,37 @@
 const passport = require('passport');
+const User = require('../models/user');
 const JwtStrategy = require('passport-jwt').Strategy;
 const { ExtractJwt } = require('passport-jwt');
+const LocalStrategy = require('passport-local').Strategy;
 
-passport.use(new JwtStrategy({},() => {
-   // jwtFromRequest: ,
-  //  secretOrKey: process.env.JWT_KEY,
+passport.use(new JwtStrategy({}, () => {
+    // jwtFromRequest: ,
+    //  secretOrKey: process.env.JWT_KEY,
 }))
+
+
+passport.serializeUser(function (user, cb) {
+    cb(null, user.id);
+});
+
+passport.deserializeUser(function (id, cb) {
+    user.findById(id, function (err, user) {
+        cb(err, user);
+    });
+});
+
+// FOR LOGIN LOCAL
+passport.use(new LocalStrategy(
+    function (username, password, done) {
+        User.findOne({ email: email }, function (err, user) {
+            if (err) { return done(err); }
+            if (!user) {
+                return done(null, false, { message: 'Incorrect username.' });
+            }
+            if (!user.validPassword(password)) {
+                return done(null, false, { message: 'Incorrect password.' });
+            }
+            return done(null, user);
+        });
+    }
+));
